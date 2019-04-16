94d01e344a920e3aaa917b4f4669e1c9cb747d06
Osnat Ackerman <osnatackerman0376@gmail.com>
2019-04-16 22:57:15 +0300

    AutoPilot

---
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotViewModel : BaseNotify
+    {
+        private string color = "White";
+        private bool isWrite = false;
+        private string content = "";
+
+        public string BackgroundColor
+        {
+            get {
+                if (content != "")
+                {
+                    return "Pink";
+                } else
+                {
+                    return "White";
+                }
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+                NotifyPropertyChanged("Content");
+            }
+        }
+
+    }
+}
